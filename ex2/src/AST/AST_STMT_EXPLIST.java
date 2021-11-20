aaa5c373ae4a8706f964d7e53b578e37c46d1ab5
Peleg Vismonsky <peleg.vismonsky@ibm.com>
2021-11-20 11:40:39 +0200

    stmt ast

---
+++ ex2/src/AST/AST_STMT_EXPLIST.java
@@ -0,0 +1,70 @@
+package AST;
+
+public class AST_STMT_EXPLIST extends AST_STMT
+{
+	/****************/
+	/* DATA MEMBERS */
+	/****************/
+	public AST_VAR var;
+    public AST_VAR_SIMPLE id;
+    public AST_EXPLIST exps;
+
+	/******************/
+	/* CONSTRUCTOR(S) */
+	/******************/
+	public AST_STMT_EXPLIST(AST_VAR var, AST_VAR_SIMPLE id, AST_EXPlist exps)
+	{
+		/******************************/
+		/* SET A UNIQUE SERIAL NUMBER */
+		/******************************/
+		SerialNumber = AST_Node_Serial_Number.getFresh();
+
+		/***************************************/
+		/* PRINT CORRESPONDING DERIVATION RULE */
+		/***************************************/
+		if (var != null) System.out.print("====================== stmt -> var.id (exp list); \n");
+		if (var == null) System.out.print("====================== stmt -> id (exp list);\n");
+
+		/*******************************/
+		/* COPY INPUT DATA NENBERS ... */
+		/*******************************/
+		this.var = var;
+        this.id = id;
+        this.exps = exps;
+		
+	}
+
+	/******************************************************/
+	/* The printing message for a statement list AST node */
+	/******************************************************/
+	public void PrintMe()
+	{
+		/**************************************/
+		/* AST NODE TYPE = AST STATEMENT LIST */
+		/**************************************/
+		System.out.print("AST NODE STMT EXP LIST\n");
+
+		/*************************************/
+		/* RECURSIVELY PRINT HEAD + TAIL ... */
+		/*************************************/
+		if (var != null) var.PrintMe();
+        if (id != null) id.PrintMe();
+        if (exps != null) exps.PrintMe();
+
+		/**********************************/
+		/* PRINT to AST GRAPHVIZ DOT file */
+		/**********************************/
+		AST_GRAPHVIZ.getInstance().logNode(
+			SerialNumber,
+			"STMT\nEXPLIST\n");
+		
+		/****************************************/
+		/* PRINT Edges to AST GRAPHVIZ DOT file */
+		/****************************************/
+		if (var != null) AST_GRAPHVIZ.getInstance().logEdge(SerialNumber,var.SerialNumber);
+        if (id != null) AST_GRAPHVIZ.getInstance().logEdge(SerialNumber,id.SerialNumber);
+        if (exps != null) AST_GRAPHVIZ.getInstance().logEdge(SerialNumber,exps.SerialNumber);
+		
+	}
+	
+}
