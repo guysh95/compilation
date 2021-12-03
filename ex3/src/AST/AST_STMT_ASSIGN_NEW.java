5e061d5e4707788d477c59e248a428b243b667b8
Ido Gazit <ido.ccmr@gmail.com>
2021-12-03 14:44:11 +0200

    Added SemantMe functions to AST files

--- ex3/src/AST/AST_STMT_ASSIGN_NEW.java
+++ ex3/src/AST/AST_STMT_ASSIGN_NEW.java
@@ -1,5 +1,7 @@
 package AST;
 
+import TYPES.*;
+
 public class AST_STMT_ASSIGN_NEW extends AST_STMT
 {
 	public AST_VAR var;
@@ -58,5 +60,20 @@ public class AST_STMT_ASSIGN_NEW extends AST_STMT
         if (var != null) AST_GRAPHVIZ.getInstance().logEdge(SerialNumber,var.SerialNumber);
         if (exp != null) AST_GRAPHVIZ.getInstance().logEdge(SerialNumber,exp.SerialNumber);
 			
+	}
+
+	public TYPE SemantMe()
+	{
+		TYPE t1 = null;
+		TYPE t2 = null;
+
+		if (var != null) t1 = var.SemantMe();
+		if (exp != null) t2 = exp.SemantMe();
+
+		if (t1 != t2)
+		{
+			System.out.format(">> ERROR [%d:%d] type mismatch for var := exp\n",6,6);
+		}
+		return null;
 	}
 }
