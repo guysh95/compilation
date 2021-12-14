59a2c18b188304398257226aca7eec983134839b
guysh95 <shnaiderguy@gmail.com>
2021-12-14 23:11:00 +0200

    fixed nil arg instead of class to function bug

--- ex3/src/AST/AST_STMT_EXPLIST.java
+++ ex3/src/AST/AST_STMT_EXPLIST.java
@@ -84,6 +84,7 @@ public class AST_STMT_EXPLIST extends AST_STMT
 		TYPE_LIST texps = null;
 
 		// only if we have var
+		// if var is not null this is a method call
 		if (var != null){
 			t1 = var.SemantMe(scope);
 			if (t1.isClass() == false)
@@ -118,19 +119,13 @@ public class AST_STMT_EXPLIST extends AST_STMT
 					if (texps.head == null){
 						System.out.format(">> ERROR missing arguments for function\n");
 						throw new lineException(Integer.toString(this.row));
-						//System.exit(0);
 					}
-					if (it.head != texps.head) {
-						System.out.format(">> ERROR parameters for function are not equal\n");
-						throw new lineException(Integer.toString(this.row));
-						//System.exit(0);
-					}
+					checkArgument(it.head, texps.head);
 					texps = texps.tail;
 				}
 				if (texps.head != null){
 					System.out.format(">> ERROR to many parameters for function\n");
 					throw new lineException(Integer.toString(this.row));
-					//System.exit(0);
 				}
 			}
 
@@ -138,9 +133,10 @@ public class AST_STMT_EXPLIST extends AST_STMT
 
 			return null;
 		} else { // var is null
-			System.out.println("We are in stmt calling " + id);
+			// this is a function call
+			// System.out.println("We are in stmt calling " + id);
 			t2 = SYMBOL_TABLE.getInstance().find(id);
-			System.out.println(id + " is of type " + t2 + " in AST_STMT_EXPLIST");
+			// System.out.println(id + " is of type " + t2 + " in AST_STMT_EXPLIST");
 			if (t2 == null) {
 				System.out.format(">> ERROR no function named %s\n", id);
 				throw new lineException(Integer.toString(this.row));
@@ -148,38 +144,66 @@ public class AST_STMT_EXPLIST extends AST_STMT
 			if(! t2.getClass().getSimpleName().equals("TYPE_FUNCTION")){
 				System.out.format(">> ERROR provided explist although this is not a function");
 				throw new lineException(Integer.toString(this.row));
-				//System.exit(0);
 			}
 			System.out.println("We found " + id);
 			TYPE_FUNCTION t3 = (TYPE_FUNCTION) t2;
 			System.out.println("we converted " + t2 + " to " + t3);
 			if (exps != null){
 				texps = exps.getTypes(null);
-				System.out.println("we got texps");
+				System.out.println("we got " + id + " arguments typle list");
 				for (TYPE_LIST it=t3.params;it != null;it=it.tail) {
-					System.out.println("1" + texps.head.name);
-					System.out.println("2" + texps.tail);
+					System.out.println("1 argument is: " + texps.head.name);
+					System.out.println("2 pointer to rest or arguments is " + texps.tail);
 					if (texps.head == null){
 						System.out.format(">> ERROR missing arguments for function\n");
 						throw new lineException(Integer.toString(this.row));
-						//System.exit(0);
 					}
-					if (it.head != texps.head) {
-						System.out.format(">> ERROR parameters for function are not equal\n");
-						throw new lineException(Integer.toString(this.row));
-						//System.exit(0);
-					}
+					checkArgument(it.head, texps.head);
 					texps = texps.tail;
 				}
 				System.out.println("We finished the loop");
 				if (texps != null){
 					System.out.format(">> ERROR to many parameters for function\n");
 					throw new lineException(Integer.toString(this.row));
-					//System.exit(0);
 				}
 			}
 			return null;
 		}
 	}
 
+	public void checkArgument(TYPE param, TYPE arg) {
+		if (param != arg) {
+			boolean isOk = false;
+			System.out.println("they are not equal");
+			if (param.isArray() && arg == TYPE_NIL.getInstance())
+				isOk = true;
+			// check if they are class
+			System.out.println(param.isClass() + " and param is " + param);
+			System.out.println(arg.isClass() + " and arg is " + arg);
+			if (param.isClass() && arg.isClass()) {
+				System.out.println("We are here");
+				if (arg == TYPE_NIL.getInstance()) {
+					System.out.println("argument is nill");
+					isOk = true;
+				}
+				else {
+					TYPE_CLASS funcExpected = (TYPE_CLASS) param;
+					TYPE_CLASS argDeliverd = (TYPE_CLASS) arg;
+					for(TYPE_CLASS dadOfIns = argDeliverd.father; dadOfIns != null; dadOfIns = dadOfIns.father){
+						if (dadOfIns == funcExpected){
+							System.out.println("argument is subclass of paramter");
+							isOk = true;
+							break;
+						}
+					}
+				}
+			}
+			System.out.println("isOk is " + isOk);
+			if (!isOk) {
+				System.out.println("isOk is " + isOk);
+				System.out.format(">> ERROR parameters for function are not equal\n");
+				throw new lineException(Integer.toString(this.row));
+			}
+		}
+	}
 }
